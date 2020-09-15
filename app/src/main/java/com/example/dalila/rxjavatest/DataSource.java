34a619a5d5173f7151710e7118b0c1e68867f7e4
Dalila Isanovic <dalila.isan@gmail.com>
2020-09-15 21:49:34 +0200

    creating observers and observables

--- app/src/main/java/com/example/dalila/rxjavatest/DataSource.java
+++ app/src/main/java/com/example/dalila/rxjavatest/DataSource.java
@@ -11,6 +11,10 @@ public class DataSource {
         tasks.add(new Task("Walk the dog",false,4 ));
         tasks.add(new Task("Do homework",false,3 ));
         tasks.add(new Task("Unload the dishwasher",true,2 ));
+        tasks.add(new Task("Take out the trash",true,5 ));
+        tasks.add(new Task("Walk the dog",false,4 ));
+        tasks.add(new Task("Do homework",false,3 ));
+        tasks.add(new Task("Unload the dishwasher",true,2 ));
         return tasks;
     }
 }
--- app/src/main/java/com/example/dalila/rxjavatest/MainActivity.java
+++ app/src/main/java/com/example/dalila/rxjavatest/MainActivity.java
@@ -2,12 +2,69 @@ package com.example.dalila.rxjavatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Predicate;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Throwable {
+                        Log.d(TAG, "test: " + Thread.currentThread().getName());
+                        /*freezing the background thread (the thread the work is being
+                        done on - Schedulers.io() in subscribeOn())
+                        */
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        //only show tasks that are complete
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        //creating an observer that's subscribing to the observable
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            //called as soon as the observer's subscribed to
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: called");
+            }
+
+            @Override
+            //called as the observer iterated through the obervables
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: called " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            //called when the process is complete
+            public void onComplete() {
+                Log.d(TAG, "onComplete: called");
+            }
+        });
     }
 }
